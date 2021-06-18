62702a720e70bbf9db9caf4a73a80ef3d1b8f7ac
BRAD JONES <ttgbjo27@gmail.com>
2021-06-18 00:45:18 -0500

    added task 5

--- index.js
+++ index.js
@@ -208,7 +208,20 @@ class Airplane {
           + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
           + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
   */
- class ProjectManager {
+ 
+  class ProjectManager extends Instructor{
+    constructor(attributes){
+        super(attributes)
+        this.gradClassName = attributes.gradClassName;
+        this.favInstructor = attributes.favInstructor;
+    }
+    standUp(slackChannel){
+
+    }
+
+    debugsCode(student, object){
+      return `${name} debugs ${student.name}'s code on ${subject}`;
+    }
      
  }
   /*
