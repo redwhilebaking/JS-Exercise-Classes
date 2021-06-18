9f64e6fc72e6a4a9484af1e3f165752137605afe
BRAD JONES <ttgbjo27@gmail.com>
2021-06-18 00:05:11 -0500

    added task 2, 3 and 4

--- index.js
+++ index.js
@@ -56,12 +56,9 @@ class Airplane {
        
      }
 
-     capacity(){
-       if(this.stomach.length === 10){
-         return "stomach is full"
-       }
+     
 
-     }
+     
 
     poop(){
        this.stomach = [];
@@ -98,6 +95,14 @@ class Airplane {
       this.tank = (this.tank + gallons);
     }
 
+    drive(distance){
+      this.tank = distance - (distance * this.milesPerGallon);
+      this.odometer += distance;
+      if(this.tank < this.milesPerGallon * distance){
+        this.tank = 0;
+        return `I ran out of fuel at ${this.odometer} miles!`;
+      }
+    }
 
   }
   
@@ -114,8 +119,18 @@ class Airplane {
           + {name} and {location} of course come from the instance's own properties.
   */
  class Lambdasian {
-    
+   constructor(student){
+    this.name = student.name;
+    this.age = student.age;
+    this.location = student.location;
+
+   }
+   speak(){
+    return `Hello my name is ${this.name}, I am from ${this.location}.`; 
+   }
   }
+    
+  
   
   /*
     TASK 4
@@ -131,9 +146,22 @@ class Airplane {
           + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
           + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
   */
- class Instructor {
+ class Instructor extends Lambdasian{
+  constructor(attributes){
+    super(attributes)
+    this.specialty = attributes.specialty;
+    this.favLanguage = attributes.favLanguage;
+    this.catchPhrase = attributes.catchPhrase;
+  }
 
- }
+  demo(subject){
+    return `Today we are learning about ${subject}`;
+  }
+
+  grade(student, subject){
+    return `${student.name} receives a perfect score on ${subject}`;
+  }
+  }
   /*
     TASK 5
       - Write a Student class extending Lambdasian.
